2616bb72c15020eef3ec1fd54b6328909697851e
jaynps <2202097@sit.singaporetech.edu.sg>
2024-10-04 00:25:44 +0800

    updated tts functionality
    Using gtts instead of pyttsx3 library
    Fixed issue with playing and clearing TTS audio files after playback by adding os.remove() to delete temporary files.
    Added session-based chat history handling.
    Implemented automatic text-to-speech for responses.
    Added adjustable playspeed
    Added downloadable audio feature for text-to-speech responses

--- utils/text_to_speech.py
+++ utils/text_to_speech.py
@@ -1,9 +1,8 @@
-import pyttsx3
-
-def speak_text(text:str):
-    engine = pyttsx3.init()
-    engine.say(text)
-    engine.runAndWait()
-
-    return True
-
+from gtts import gTTS
+import uuid
+# Function to convert text to speech using gTTS
+def text_to_speech(text):
+    tts = gTTS(text=text, lang='en')
+    filename = f"audio_{uuid.uuid4()}.mp3"  # Generate a unique filename
+    tts.save(filename)
+    return filename  # Return the filename for playback
